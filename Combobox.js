136b01028ba976ce53ef40f4f3a0206bebdc836e
Bruno Bruzzano <brunano21@gmail.com>
2016-12-05 16:01:13 +0000

    Focus any previously focus list's item when opening the dropdown.

--- Combobox.js
+++ Combobox.js
@@ -1035,6 +1035,9 @@ define([
 			// Since List is in focus-less mode, it does not give focus to
 			// navigated items, thus the browser does not autoscroll.
 			// TODO: see deliteful #498
+			if (!item) {
+				item = this.list.navigatedDescendant;
+			}
 
 			if (!item) {
 				var selectedItems = this.list.selectedItems;
